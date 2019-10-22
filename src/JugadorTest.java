ddc875f221258c15a94d44cc2a1f4ff237501afe
Adonis González <adions025@gmail.com>
2019-10-22 09:11:45 +0200

    test jugador

--- src/JugadorTest.java
+++ src/JugadorTest.java
@@ -6,7 +6,19 @@ class JugadorTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+	}
+	
+	@Test
+	void testConstructor() {
+		
+		Jugador jugador1 = new Jugador(true);
+		boolean piezaNegra = jugador1.isColor();
+		assertTrue(piezaNegra);
+		
+		Jugador jugador2 = new Jugador(false);
+		boolean piezaBlanca = jugador2.isColor();
+		assertFalse(piezaBlanca);
 	}
 
 }
