4cf8b7eda621a9e02b3d7007dd7c44863abd60d0
Adonis González <adions025@gmail.com>
2019-10-30 00:32:50 +0100

    test get tablero

--- src/juego/Tablero.java
+++ src/juego/Tablero.java
@@ -1,5 +1,29 @@
 package juego;
+
 public class Tablero {
+	private Pieza [][] tablero;
 	
+	public Tablero() {
+		this.tablero = new Pieza[8][8];
+	}
+	
+	public String toString() 
+	{
+		String frame = "   1  2  3  4  5  6  7  8\n";
+		for (int i = 0; i < tablero.length; i++) {
+			frame += ""+(i+1);
+			for (int j = 0; j < tablero.length; j++) {
+				if (this.tablero[i][j] == null) {
+					frame += "  -";
+				} 
+				else {
+
+				}
+			}
+			frame += "\n";
+		}
+		
+		return frame;
+	}
 	
 }
--- src/test/TableroTest.java
+++ src/test/TableroTest.java
@@ -3,12 +3,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import juego.Juego;
 import juego.Tablero;
 
 class TableroTest {
 
 	@Test
-	void test() {
+	void testContructorTablero() {
+		Juego juego = new Juego();
+		
+		Tablero tablero;
+		tablero = juego.getTablero();
+		
+		String tableroEsperado = ""
+				+ "   1  2  3  4  5  6  7  8\n" + 
+				"1  -  -  -  -  -  -  -  -\n" + 
+				"2  -  -  -  -  -  -  -  -\n" + 
+				"3  -  -  -  -  -  -  -  -\n" + 
+				"4  -  -  -  -  -  -  -  -\n" + 
+				"5  -  -  -  -  -  -  -  -\n" + 
+				"6  -  -  -  -  -  -  -  -\n" + 
+				"7  -  -  -  -  -  -  -  -\n" + 
+				"8  -  -  -  -  -  -  -  -";
+		
+		assertEquals(tableroEsperado, tablero);
 		
 	}
 	
