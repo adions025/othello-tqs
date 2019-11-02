1ed766d2943c2cd5e159f6df3346befec269e469
JonatanLuzon <jonatan.luzon@e-campus.uab.cat>
2019-11-02 01:03:51 +0100

    Corrección de la función ponerPieza(). cambiada mas la condición if de ors a ands

--- src/juego/Jugador.java
+++ src/juego/Jugador.java
@@ -16,7 +16,7 @@ public class Jugador {
 
 	public Boolean ponerPieza(int fila, int columna) {
 		if((Juego.getTablero().getTablero()[fila][columna] == null) && 
-				(fila < 0 || fila > 7) && (columna < 0 || columna > 7)) {
+				(fila < 0 && fila > 7) && (columna < 0 && columna > 7)) {
 			Juego.getTablero().getTablero()[fila][columna] = new Pieza(this.color);
 			return true;
 		} else {
